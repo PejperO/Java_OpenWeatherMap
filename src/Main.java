6a34b8c0fefa833a0b6878f166caa907f5f3be82
Sumii <64231313+PejperO@users.noreply.github.com>
2022-05-11 01:36:52 +0200

    FileStream

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
+import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         String API_KEY = "1efc05f8c068f443b4d82d6678698c08";
-        String latitude = "52.237049", longitude = "21.017532"; //Warsaw - test data
-        URL weatherURL = new URL("https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude={part}&appid="+API_KEY);
+        String city, latitude = null, longitude= null;
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(weatherURL.openStream(), StandardCharsets.UTF_8));
+        /*
+        FileInputStream fileO = new FileInputStream("data/city_coords.dat");
+        BufferedInputStream buffO = new BufferedInputStream(fileO);
+        ObjectInputStream ObjO = new ObjectInputStream(buffO);
+
+        Data data = (Data)ObjO.readObject();
+        city = data.city;
+        latitude = data.latitude;
+        longitude = data.longitude;
+        ObjO.close();
+        System.out.println(city + latitude + longitude);
+        */
+        /*
+        Scanner scanner = new Scanner("data/city_coords.dat");
+        String linia1 = scanner.nextLine();
+        System.out.println(linia1);
+        */
+        File file = new File("data/city_coords.dat");
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line1;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter a city");
+        city = keyboard.nextLine();
+        while ((line1 = br.readLine()) != null) {
+            String[] parts = line1.split(", ");
+            if(Objects.equals(parts[0], city)){
+                latitude = parts[1];
+                longitude = parts[2];
+            }
+        }
+
+        URL weatherURL= new URL("https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude=hourly,daily&appid="+API_KEY);;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(weatherURL.openStream(), StandardCharsets.UTF_8));;
 
         String line;
         StringBuilder stringBuilder = new StringBuilder();
+
         while((line = bufferedReader.readLine())!=null)
             stringBuilder.append(line);
         line = stringBuilder.toString();
@@ -22,4 +55,3 @@ public class Main {
         System.out.println(line);
     }
 }
-
