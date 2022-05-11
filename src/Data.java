c038f8dabfd6ea4c36dc505adc04c274883bbf4a
Sumii <64231313+PejperO@users.noreply.github.com>
2022-05-11 17:50:33 +0200

    Aesthetic Corrections

--- src/Data.java
+++ src/Data.java
@@ -3,47 +3,73 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Data implements Serializable {
+    //String weather = "daily", city= "warsaw", latitude= "52.237049", longitude= "21.017532";    // test data - so that I do not have to enter them every time I test the program
     String weather, city, latitude, longitude;
 
-    public void Data(){
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Enter the path to the file with geographical coordinates of cities: ");
+    Scanner keyboard = new Scanner(System.in);
+    FileReader fileReader = null;
+
+    public void data(){
+        setFileReader();
+        setWeather();
+        try {
+            setLatLon();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setFileReader(){
+        System.out.print("Enter the path to the file with geographical coordinates of cities (leave empty to get default): ");
         String filePath = keyboard.nextLine();
 
-        File file = new File(filePath); // data/city_coords.dat
-        FileReader fileReader = null;
+        if(Objects.equals(filePath, ""))
+            filePath = "data/city_coords.dat";
+
+        File file = new File(filePath);
         try {
             fileReader = new FileReader(file);
         } catch (FileNotFoundException e) {
             System.out.println("No such file or directory");
-            e.printStackTrace();
+            setFileReader();
         }
+    }
 
-        while(true){
-            System.out.print("Enter the type of weather forecast (daily / hourly): ");
-            weather = keyboard.nextLine();
-            if(Objects.equals(weather, "daily") || Objects.equals(weather, "hourly"))
-                break;
-            else
+    private void setWeather(){
+        System.out.print("Enter the type of weather forecast (daily / hourly): ");
+        weather = keyboard.nextLine();
+
+        switch (weather) {
+            case "daily" -> weather = "hourly";
+            case "hourly" -> weather = "daily";
+            default -> {
                 System.out.println("No such type");
+                setWeather();
+            }
         }
+    }
 
+    private void setLatLon() throws IOException {
         System.out.print("Enter a city: ");
         city = keyboard.nextLine();
 
         String line;
-        assert fileReader != null;
         BufferedReader bufferedReader = new BufferedReader(fileReader);
-        try {
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] parts = line.split(", ");
-                if (Objects.equals(parts[0], city)) {
-                    latitude = parts[1];
-                    longitude = parts[2];
-                }
+
+        //boolean get = false;
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] parts = line.split(", ");
+            if (Objects.equals(parts[0], city)) {
+                latitude = parts[1];
+                longitude = parts[2];
+                //get = true;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        /*
+        if(!get){
+            System.out.println("No such city in the file or directory");
+            setLatLon();
+        }
+        */
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
         String API_KEY = "1efc05f8c068f443b4d82d6678698c08";
 
         Data data = new Data();
-        data.Data();
+        data.data();
 
-        URL weatherURL= new URL("https://api.openweathermap.org/data/2.5/onecall?lat="+data.latitude+"&lon="+data.longitude+"&exclude="+data.weather+"&appid="+API_KEY);;
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(weatherURL.openStream(), StandardCharsets.UTF_8));;
+        URL weatherURL= new URL("https://api.openweathermap.org/data/2.5/onecall?lat="+data.latitude+"&lon="+data.longitude+"&exclude=current,minutely,alerts,"+data.weather+"&appid="+API_KEY);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(weatherURL.openStream(), StandardCharsets.UTF_8));
 
         StringBuilder stringBuilder = new StringBuilder();
         String line;
