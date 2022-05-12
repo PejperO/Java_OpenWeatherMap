038ecbf362fb0ddcf6e30a25a128c37972ce70aa
Sumii <64231313+PejperO@users.noreply.github.com>
2022-05-12 21:07:17 +0200

    Data Operations 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class Main {
     public static void main(String[] args){
         Service service = new Service();
-        System.out.println(service.getWeather());
+        JsonParser parser = new JsonParser();
+
+        String json = service.getWeather();
+
+        JsonObject object = (JsonObject) parser.parse(json);
+        JsonArray daily = (JsonArray) object.get("daily");
+        JsonObject dailyObject = (JsonObject) daily.get(0);
+        JsonObject temp = (JsonObject) dailyObject.get("temp");
+
+        int tempMin = (int) Math.round(Double.parseDouble(temp.get("min").toString()) - 273.15);
+        int tempMax = (int) Math.round(Double.parseDouble(temp.get("max").toString()) - 273.15);
+        int tempAverage  = (int) Math.round(Double.parseDouble(temp.get("max").toString()) - 273.15);
+
+        String msg =    "Min: " + tempMin + "°C\n" +
+                        "Max: " + tempMax + "°C\n" +
+                        "Average: " + tempAverage + "°C\n";
+
+        System.out.println(msg);
     }
 }
