5248036a178d137d2df2be22954a09a28147ba58
Sumii <64231313+PejperO@users.noreply.github.com>
2022-05-12 20:08:54 +0200

    Create Service.java

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,7 @@
-import com.google.gson.*;
-
-import java.io.*;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        String API_KEY = "1efc05f8c068f443b4d82d6678698c08";
-
-        Data data = new Data();
-        //data.data();
-
-        URL weatherURL= new URL("https://api.openweathermap.org/data/2.5/onecall?lat="+data.latitude+"&lon="+data.longitude+"&exclude=current,minutely,alerts,"+data.weather+"&appid="+API_KEY);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(weatherURL.openStream(), StandardCharsets.UTF_8));
-
-        StringBuilder stringBuilder = new StringBuilder();
-        String line;
-
-        while((line = bufferedReader.readLine())!=null)
-            stringBuilder.append(line);
-        //String json = stringBuilder.toString();
-
-        JsonParser parser = new JsonParser();
-        JsonObject object = (JsonObject) parser.parse(stringBuilder.toString());
-        JsonArray rates = (JsonArray) object.get("rates");
-        JsonObject ratesObject = (JsonObject)rates.get(0);
-        JsonPrimitive mid = (JsonPrimitive) ratesObject.get("mid");
-        System.out.println(mid.toString());
-
-        /*
-        Object obj = new JsonParser().parse(json);
-        JsonObject jo = (JsonObject) obj;
-        Map daily = (Map) jo.get("daily");
-        Set zero = daily.keySet();
-        System.out.println(zero);
-        */
-
-        //System.out.print(line);
+    public static void main(String[] args){
+        Service service = new Service();
+        System.out.println(service.getWeather());
     }
 }
---
+++ src/Service.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class Service {
+    String API_KEY = "1efc05f8c068f443b4d82d6678698c08";
+    Data data = new Data();
+
+    public String getWeather(){
+        StringBuilder json = new StringBuilder();
+        data.data();
+
+        try {
+            URL weatherURL= new URL("https://api.openweathermap.org/data/2.5/onecall?lat="+data.latitude+"&lon="+data.longitude+"&exclude=current,minutely,alerts,"+data.weather+"&appid="+API_KEY);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(weatherURL.openStream(), StandardCharsets.UTF_8));
+            String line;
+
+            while((line = bufferedReader.readLine())!=null)
+                json.append(line);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return json.toString();
+    }
+}
